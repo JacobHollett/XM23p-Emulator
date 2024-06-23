a5c64b6171c1f837143f756120aae826dc741072
Jacob Hollett <hollettjacobs@gmail.com>
2024-06-23 20:21:46 -0300

    Refactored PSW setting

--- instructions.c
+++ instructions.c
@@ -7,14 +7,9 @@ unsigned char overflowTable[2][2][2] = {{{0,1},{0,0}},
                                         {{0,0},{1,0}}};
 
 
-//Handles the add and addc
-//carry is passed in as 0 for add
-void ADD(int RC, int WB, int SC, int D, int carry){
-
-    twoWord tempResult;
+void setPSW(twoWord tempResult, int WB, int RC, int SC, int D){
 
     if (WB){
-        tempResult.value = regFile[RC][SC].bytes[0] + regFile[0][D].bytes[0] + carry;
         psw.bit[0].b0 = tempResult.bit[1].b0;   //Carry
         if (!tempResult.bytes[0])               //Zero
             psw.bit[0].b1 = 1;
@@ -23,18 +18,34 @@ void ADD(int RC, int WB, int SC, int D, int carry){
         psw.bit[0].b2 = tempResult.bit[0].b7;   //Negative
         psw.bit[0].b4 = 
         overflowTable[regFile[RC][SC].bit[0].b7][regFile[0][D].bit[0].b7][tempResult.bit[0].b7];
-        regFile[0][D].bytes[0] = tempResult.bytes[0];
     }
     else{
-        tempResult.value = regFile[RC][SC].word + regFile[0][D].word + carry;
         psw.bit[0].b0 = tempResult.bit[2].b0;   //Carry
         if (!tempResult.word[0])                //Zero
             psw.bit[0].b1 = 1;
         else
-            psw.bit[0].b1 = 0; 
+            psw.bit[0].b1 = 0;
         psw.bit[0].b2 = tempResult.bit[1].b7;   //Negative
         psw.bit[0].b4 = 
         overflowTable[regFile[RC][SC].bit[1].b7][regFile[0][D].bit[1].b7][tempResult.bit[1].b7];
+    }
+}
+
+
+//Handles the add and addc
+//carry is passed in as 0 for add
+void ADD(int RC, int WB, int SC, int D, int carry){
+
+    twoWord tempResult;
+
+    if (WB){
+        tempResult.value = regFile[RC][SC].bytes[0] + regFile[0][D].bytes[0] + carry;
+        setPSW(tempResult, WB, RC, SC, D);
+        regFile[0][D].bytes[0] = tempResult.bytes[0];
+    }
+    else{
+        tempResult.value = regFile[RC][SC].word + regFile[0][D].word + carry;
+        setPSW(tempResult, WB, RC, SC, D);
         regFile[0][D].word = tempResult.word[0];
     }
 }
@@ -48,31 +59,14 @@ void SUB(int RC, int WB, int SC, int D, int carry, int flag){
         tempResult.value = regFile[0][D].bytes[0] - regFile[RC][SC].bytes[0];
         if(flag)
             tempResult.value = tempResult.value - 1 + carry;
-        
-        psw.bit[0].b0 = tempResult.bit[1].b0;   //Carry
-        if (!tempResult.bytes[0])               //Zero
-            psw.bit[0].b1 = 1;
-        else
-            psw.bit[0].b1 = 0;
-        psw.bit[0].b2 = tempResult.bit[0].b7;   //Negative
-        psw.bit[0].b4 = 
-        overflowTable[regFile[RC][SC].bit[0].b7][regFile[0][D].bit[0].b7][tempResult.bit[0].b7];
+        setPSW(tempResult, WB, RC, SC, D);
         regFile[0][D].bytes[0] = tempResult.bytes[0];
-        
     }
     else{
         tempResult.value = regFile[0][D].word - regFile[RC][SC].word;
         if(flag)
             tempResult.value = tempResult.value - 1 + carry;
-        
-        psw.bit[0].b0 = tempResult.bit[2].b0;   //Carry
-        if (!tempResult.word[0])                //Zero
-            psw.bit[0].b1 = 1;
-        else
-            psw.bit[0].b1 = 0; 
-        psw.bit[0].b2 = tempResult.bit[1].b7;   //Negative
-        psw.bit[0].b4 = 
-        overflowTable[regFile[RC][SC].bit[1].b7][regFile[0][D].bit[1].b7][tempResult.bit[1].b7];
+        setPSW(tempResult, WB, RC, SC, D);
         regFile[0][D].word = tempResult.word[0];
     }
 }
@@ -109,31 +103,10 @@ void CMP(int RC, int WB, int SC, int D){
     
     twoWord tempResult;
 
-    if (WB){
-        tempResult.value = regFile[0][D].bytes[0] - regFile[RC][SC].bytes[0];
-        
-        psw.bit[0].b0 = tempResult.bit[1].b0;   //Carry
-        if (!tempResult.bytes[0])               //Zero
-            psw.bit[0].b1 = 1;
-        else
-            psw.bit[0].b1 = 0;
-        psw.bit[0].b2 = tempResult.bit[0].b7;   //Negative
-        psw.bit[0].b4 = 
-        overflowTable[regFile[RC][SC].bit[0].b7][regFile[0][D].bit[0].b7][tempResult.bit[0].b7];
-    }
-    else{
-        tempResult.value = regFile[0][D].word - regFile[RC][SC].word;
-
-        psw.bit[0].b0 = tempResult.bit[2].b0;   //Carry
-        if (!tempResult.word[0])                //Zero
-            psw.bit[0].b1 = 1;
-        else
-            psw.bit[0].b1 = 0; 
-        psw.bit[0].b2 = tempResult.bit[1].b7;   //Negative
-        psw.bit[0].b4 = 
-        overflowTable[regFile[RC][SC].bit[1].b7][regFile[0][D].bit[1].b7][tempResult.bit[1].b7];
-    }
-
+    if (WB) tempResult.value = regFile[0][D].bytes[0] - regFile[RC][SC].bytes[0];
+    else tempResult.value = regFile[0][D].word - regFile[RC][SC].word;
+    
+    setPSW(tempResult, WB, RC, SC, D);
 }
 
 
